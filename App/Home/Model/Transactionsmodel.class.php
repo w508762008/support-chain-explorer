88f2ead525591adc9e785e768107147c6db97eeb
w508762008 <508762008@qq.com>
2018-08-10 15:52:55 +0800

    init commit

---
+++ App/Home/Model/Transactionsmodel.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Home\Model;
+ use Think\Model\MongoModel;
+ Class TransactionsModel extends MongoModel 
+ { 
+
+ //可以是空的。
+    public  function cg(){
+        echo "cg";
+    }
+ }
+?>
